f2f322181bcc893c1deaa53da3d492d116a94195
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2020-12-08 16:03:20 +0300

    steb by step logic

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Created by Yevgeniy Goncharov, https://sys-adm.in
+# https://certbot.eff.org/lets-encrypt/centosrhel7-nginx
+
+# Install snapd
+yum install snapd
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
+
+# Update snapd and install certbot
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+
+# Simple help
+echo -e "You can use these commands:\n"
+echo "sudo certbot --nginx"
+echo "sudo certbot certonly --nginx"
+
+echo -e "Check automatic renewal:\n"
+echo "sudo certbot renew --dry-run"
+
+echo -e "You can find sheduled tasks in the lications:\n"
+echo -e "
+/etc/crontab/
+/etc/cron.*/*
+systemctl list-timers
+"
+
+
